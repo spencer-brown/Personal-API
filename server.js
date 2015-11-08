07a016bd1ddc4f8e464f691c1b0712523f0198d4
Mehak <vohram@purdue.edu>
2015-11-08 06:31:40 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,12 +2,11 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type':'text/html'});
-	res.end('<h1>Hello, World</h1>');
+	res.end('<h2>A Personal API for Mehak Vohra</h2>');
 });
 
 var port = Number(process.env.PORT || 3000);
 
 
 server.listen(port);
-
-
+console.log('API server started on port ' + port);
