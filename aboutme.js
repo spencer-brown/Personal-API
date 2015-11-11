a1fb7accc271bc70aefeea449272ac90caf0af8a
Mehak <vohram@purdue.edu>
2015-11-10 20:18:37 -0500

    added about med

---
+++ aboutme.js
@@ -0,0 +1,25 @@
+{
+	"Basic Information" : {
+	       "Name" : "Mehak Vohra",
+	       "Age": "19 years old",
+	       "Location": "West Lafayette"
+	}, 
+	"Schooling": {
+	      "College": "Purdue University",
+	      "Year": "Sophomore",
+	      "Major": "Computer Science"
+	}, 
+	"Activities": {
+	      "Company": "Jamocha Media",
+	      "Company Position": "CEO/Founder",
+	      "hackathon": "BoilerMake",
+	      "The Anvil": "Social Media Lead"
+	},
+	"Social Media": {
+	      "Facebook": "watthemehak",
+	      "Twitter": "@watthemehak",
+	      "Instagram": "watthemehak",
+	      "Linked In": "mehakvohra
+	}
+}
+}
