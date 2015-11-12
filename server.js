063533f6f0ecd15ed1cc7dfca6df088a1f23bf27
Mehak <vohram@purdue.edu>
2015-11-12 14:17:46 -0500

    basic changes

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ app.get('/', function (req, res) {
 
 var port = Number(process.env.PORT || 3000);
 
-app.get('/name', function(req, res) {
-	res.json(name)
-});
-
 
 //Starting a Server
 app.listen(port);
